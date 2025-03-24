6cb56522e19b74601f6749d2de8bb5cc478a9388
Marc Mortensen <marcmortensen77@gmail.com>
2025-03-24 17:30:34 +0100

    feat: add query parsms and filtering results

--- apps/web/nuxt.config.ts
+++ apps/web/nuxt.config.ts
@@ -15,6 +15,7 @@ export default defineNuxtConfig({
     "@nuxt/image",
     "@nuxt/content",
     "@nuxt/ui",
+    "@vueuse/nuxt",
   ],
   vite: {
     plugins: [tailwindcss()],
--- apps/web/pages/organizations.vue
+++ apps/web/pages/organizations.vue
@@ -1,21 +1,88 @@
 <script setup lang="ts">
 const route = useRoute();
-const { data: orgs } = await useAsyncData(route.path, () => {
-  return queryCollection("organizations").all();
+
+const typeOfAnimal = ["cats", "dogs"];
+
+const queryTransform = {
+  get: (value: string | undefined) => value?.split(",") || [],
+  set: (value: string[]) => (value.length === 0 ? undefined : value.join(",")),
+};
+
+const regions = useRouteQuery("regions", undefined, {
+  transform: queryTransform,
 });
+const animalType = useRouteQuery("animalType", undefined, {
+  transform: queryTransform,
+});
+
+const page = useRouteQuery("page", "1", {
+  mode: "push",
+  transform: { get: Number, set: String },
+});
+
+const { data } = await useAsyncData(
+  route.path,
+  async () => {
+    const dataRegions = await queryCollection("regions").all();
+    const query = queryCollection("organizations");
+
+    if (regions.value.length > 0) {
+      query.orWhere((query) => {
+        regions.value.forEach((slug) => {
+          query.where("region", "LIKE", `%${slug}%`);
+        });
+        return query;
+      });
+    }
+
+    if (animalType.value.length > 0) {
+      query.orWhere((query) => {
+        animalType.value.forEach((slug) => {
+          query.where("animalFocus", "LIKE", `%${slug}%`);
+        });
+        return query.where("animalFocus", "LIKE", `%cats&dogs%`);
+      });
+    }
+
+    const orgs = await query.limit(9).all();
+    return { orgs, regions: dataRegions };
+  },
+  { immediate: true, watch: [regions, animalType, page] }
+);
 </script>
 
 <template>
-  <div class="flex flex-col w-1/2 justify-center items-center mx-auto">
+  <div
+    v-if="data"
+    class="flex flex-col w-1/2 justify-center items-center mx-auto mt-4"
+  >
+    <div>
+      <USelectMenu
+        v-model="regions"
+        multiple
+        :items="data.regions"
+        value-key="slug"
+        label-key="name"
+        placeholder="Select a region"
+      />
+      <USelectMenu
+        v-model="animalType"
+        multiple
+        :items="typeOfAnimal"
+        :search-input="false"
+        placeholder="Select animal type"
+      />
+    </div>
+    <pre>{{ data?.orgs.length }}</pre>
     <UIcon name="i-lucide-lightbulb" class="size-5" />
     <h1 class="text-4xl font-semibold">Organizations</h1>
     <p class="text-lg text-gray-500">
       Here are some organizations that you can support
     </p>
 
-    <div class="grid grid-cols-1 sm:grid-cols-3 gap-4">
+    <div v-if="data?.orgs" class="grid grid-cols-1 sm:grid-cols-3 gap-4">
       <div
-        v-for="org in orgs"
+        v-for="org in data.orgs"
         :key="org.id"
         class="bg-white p-4 shadow rounded group relative"
       >
