a8b03762a6a97064ff028175c3007bf5f3de412c
Marc Mortensen <marcmortensen77@gmail.com>
2025-04-10 14:20:26 +0200

    feat: add error page

--- apps/web/app/error.vue
+++ apps/web/app/error.vue
@@ -2,13 +2,34 @@
 <template>
   <UApp>
     <NuxtLayout>
-      <div class="text-4xl">Error!</div>
-      <UButton
-        class="mt-4"
-        variant="outline"
-        label="Go to Home"
-        @click="$router.push('/')"
-      />
+      <div class="h-screen max-w-screen bg-white flex items-center">
+        <div
+          class="container flex flex-col-reverse md:flex-row items-center justify-center px-5 text-gray-700"
+        >
+          <div class="max-w-md">
+            <div class="text-5xl font-dark font-bold">404</div>
+            <p class="text-2xl md:text-3xl font-light leading-normal">
+              Sorry we couldn't find this page.
+            </p>
+            <p class="mb-8">
+              But dont worry, you can find plenty of other things on our
+              homepage.
+            </p>
+
+            <UButton
+              icon="i-lucide-house"
+              label="Back to homepage"
+              :to="{ name: 'index' }"
+            />
+          </div>
+          <div class="max-w-lg flex justify-center h-full w-full">
+            <SvgoAnimalsCatTripBase
+              :font-controlled="false"
+              class="h-52 w-full sm:h-96"
+            />
+          </div>
+        </div>
+      </div>
     </NuxtLayout>
   </UApp>
 </template>
