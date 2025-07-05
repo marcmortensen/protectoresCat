b9317fe50d01e001b05ba5f733d979d64377312b
Marc Mortensen <marcmortensen77@gmail.com>
2025-07-05 10:19:33 +0200

    fix: deploy to netlify

--- apps/web/nuxt.config.ts
+++ apps/web/nuxt.config.ts
@@ -2,6 +2,7 @@ import content from "@originjs/vite-plugin-content";
 import tailwindcss from "@tailwindcss/vite";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: true,
   future: {
     compatibilityVersion: 4,
   },
