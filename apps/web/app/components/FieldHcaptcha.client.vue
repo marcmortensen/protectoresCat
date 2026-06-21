714e072d50517468573f3ba22ed2a729d1422436
Marc Mortensen <marcmortensen77@gmail.com>
2026-06-21 11:10:49 +0200

    feat: enhance hCaptcha integration with light/dark mode support.

--- apps/web/app/components/FieldHcaptcha.client.vue
+++ apps/web/app/components/FieldHcaptcha.client.vue
@@ -4,17 +4,24 @@ import HCaptcha from "@hcaptcha/vue3-hcaptcha";
 const WEB3FORMS_HCAPTCHA_SITEKEY = "50b2fe65-b00b-4b9e-ad62-3ba471098be2";
 const token = defineModel<string | null>({ default: null });
 const hcaptchaRef = ref<InstanceType<typeof HCaptcha> | null>(null);
+const colorMode = useColorMode();
 
 defineExpose({ reset: () => hcaptchaRef.value?.reset() });
+
+const theme = computed(() => {
+  return colorMode.value === "dark" ? "dark" : "light";
+});
 </script>
 
 <template>
   <HCaptcha
     ref="hcaptchaRef"
+    :key="theme"
     :sitekey="WEB3FORMS_HCAPTCHA_SITEKEY"
     :re-captcha-compat="false"
     @verify="(t: string) => (token = t)"
     @expired="token = null"
     @error="token = null"
+    :theme="theme"
   />
 </template>
